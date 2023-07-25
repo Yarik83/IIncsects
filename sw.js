db2fe7c27dace537d8051500f2e7c651bde43100
Yarik83 <140487761+Yarik83@users.noreply.github.com>
2023-07-25 15:18:26 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(["index.html","style/style.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+importScripts('sw-toolbox.js'); toolbox.precache(["index.html", "ant.htm", "butterfly.htm", "spider.htm", "style.css"," style1.css","style2.css", "style3.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
